de35e5b7bd0d6a86c3bfe7f00871b377e3827cb6
Samar97 <samar19972102@gmail.com>
2017-12-25 16:08:24 +0530

    Image upload basic interface

--- PDDA/disease/views.py
+++ PDDA/disease/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 from django.http import HttpResponse
+from django.urls import reverse
+from django.template import loader
 # Create your views here.
 
+@csrf_exempt
 def home(request):
-	return HttpResponse("<h1>This is the homepage of the plant disease detection app!</h1>")
+	if request.method == 'GET':
+		template = loader.get_template('home.html')
+		return HttpResponse(template.render(request))
+	else:
+		return HttpResponseRedirect(reverse("success"))
+
+def success(request):
+	return HttpResponse("<h2>Image Upload successful</h2>")
