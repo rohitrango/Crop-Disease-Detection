63e01d107ffc1df41ea66e250683b8e1bf7ff780
rohitrango <rohit.rango@gmail.com>
2018-01-03 00:35:38 +0530

    add url for knn api

--- PDDA/disease/urls.py
+++ PDDA/disease/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^upload_image_and_get_results/', upload_image_and_get_results, name='upload_image_and_get_results'),
     url(r'^save_entry', save_entry, name='save_entry'),
     url(r'^update_crops', update_crops, name='update_crops'),
+    url(r'^get_crops', get_crop_names, name='get_crop_names'),
 ]
